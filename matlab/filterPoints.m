fccc479e0e1ae26275a969eb60e4e572d43e4ae6
Clayton Mellina <cemellina@gmail.com>
2011-11-18 14:08:33 -0800

    flushed out gesture.m for testing, filterPoints now also filters the dimensions of the data we used

--- matlab/filterPoints.m
+++ matlab/filterPoints.m
@@ -5,11 +5,11 @@ threshold = 0.1;
 
 % Filter, keeping samples with large deltas only
 points = zeros(1, 3);
-points(1,:) = origPoints(1,:);
+points(1,:) = origPoints(1,5:7);
 for k=2:origM
   % distSq = dot(origPoints(k,:), origPoints(m,:));
-  distSq = dot(origPoints(k,:), origPoints(k - 1,:));
+  distSq = dot(origPoints(k,5:7), origPoints(k - 1,5:7));
   if distSq > threshold
-     points = [points; origPoints(k,:)];
+     points = [points; origPoints(k,5:7)];
   end
 end
--- matlab/gesture.m
+++ matlab/gesture.m
@@ -9,9 +9,10 @@ for k=1:numel(training)
 end
 
 clust = computeClusters(allData, num_clusters);
-T = delaunanyn(clust);
+T = delaunayn(clust);
 
-emission_init = ones(num_clusters, );
+prior_init = 1/8 * ones(1,8);
+emission_init = ones(8, num_clusters);
 trans_init = ...
 [ 1/3 1/3 1/3 0 0 0 0 0; ...
   0 1/3 1/3 1/3 0 0 0 0; ...
@@ -20,9 +21,12 @@ trans_init = ...
   0 0 0 0 1/3 1/3 1/3 0; ...
   0 0 0 0 0 1/3 1/3 1/3; ...
   0 0 0 0 0 0 1/2 1/2; ...
-  0 0 0 0 0 0 0 0 1 ...
+  0 0 0 0 0 0 0 1 ...
 ];
 
+priors = cell(size(training));
+transmats = cell(size(training));
+obsmats = cell(size(training));
 for k=1:numel(training)
   gestureExamples = training{k};
   numExamples = numel(gestureExamples);
@@ -30,26 +34,55 @@ for k=1:numel(training)
   for l=1:numExamples
     sample{l} = dsearchn(clust, T, gestureExamples{l});
   end
-  dhmm_em(sample, trans_init, emission_init);
+  [ll_trace, prior, transmat, obsmat, iterNr] = dhmm_em(sample, prior_init, trans_init, emission_init, 'max_iter', 20);
+  priors{k} = prior;
+  transmats{k} = transmat;
+  obsmats{k} = obsmat;
 end
 
-allData = readAccelData('../data/circles/xbowSensorLog.txt');
 
-origPoints = allData(:,5:7);
-
-hold on
-scatter3(origPoints(:,1), origPoints(:,2), origPoints(:,3), 2, 'k')
-
-[IDX, C] = computeClusters(origPoints);
-
-IDX
-C
-clustSizes = zeros(dictSize, 1);
-for k=1:m
-  clustSizes(IDX(k)) = clustSizes(IDX(k)) + 1;
+% test classifier
+num_gestures = numel(testing);
+num_correct = 0;
+num_total = 0;
+for g = 1:num_gestures
+    for k = 1:numel(testing{g})
+        % discretize
+        disc_test = dsearchn(clust, T, testing{g}{k});
+        
+        % try all HMMs
+        loglik = zeros(1, num_gestures);
+        for l = 1:num_gestures
+            loglik(l) = dhmm_logprob(disc_test, priors{l}, transmats{l}, obsmats{l});
+        end
+        [val, ind] = max(loglik);
+        if ind == g
+            num_correct = num_correct + 1;
+        end
+        num_total = num_total + 1;
+    end
 end
+disp(['Accuracy of ' num2str(num_correct / num_total) ' on ' num2str(num_total) ' test samples']);
 
-maxSize = max(clustSizes);
-clustSizes = clustSizes ./ (maxSize / 10);
 
-scatter3(C(:,1), C(:,2), C(:,3), clustSizes * 5, 'b')
+% allData = readAccelData('../data/circles/xbowSensorLog.txt');
+% 
+% origPoints = allData(:,5:7);
+% 
+% hold on
+% scatter3(origPoints(:,1), origPoints(:,2), origPoints(:,3), 2, 'k')
+% 
+% [IDX, C] = computeClusters(origPoints);
+% 
+% IDX
+% C
+% clustSizes = zeros(dictSize, 1);
+% for k=1:m
+%   clustSizes(IDX(k)) = clustSizes(IDX(k)) + 1;
+% end
+% 
+% maxSize = max(clustSizes);
+% clustSizes = clustSizes ./ (maxSize / 10);
+% 
+% scatter3(C(:,1), C(:,2), C(:,3), clustSizes * 5, 'b')
+
